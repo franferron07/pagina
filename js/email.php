afb3496b55cb59fe2f3d064a463d64a201048bbd
Francisco <fran.ferron07@gmail.com>
2017-07-19 12:27:31 -0300

    update

--- js/email.php
+++ js/email.php
@@ -2,9 +2,10 @@
 if($_POST){
     $name = $_POST['nombre'];
     $email = $_POST['email'];
+	$telefono = $_POST['telefono'];
     $message = $_POST['mensaje'];
 
-	$texto = "La persona" + $name + ", cuyo email es " + $email + " te ha envíado el siguiente mensaje: " + $message; 
+	$texto = 'La persona:' + $name + ', cuyo email es:' + $email + ',de telefono:' + $telefono + ' te ha envíado el siguiente mensaje:' + $message; 
 	
 	//send email
 	mail("consultas@pekes-eventos.com.ar", "Mensaje de pagina", $texto);
--- js/request_mail.js
+++ js/request_mail.js
@@ -2,24 +2,57 @@
 
 
 $(function() {
-
-$('#enviar').click(function(){
 	
-    var data = {
-		nombre: $("#nombre").val(),
-		email: $("#email").val(),
-		mensaje: $("#mensaje").val()
-    };
-    $.ajax({
-		type: "POST",
-		url: "email.php",
-		data: data,
-		success: function(){
-			$('.success').fadeIn(1000);
+	
+	$(".numericOnly").keypress(function (e) {
+		if (String.fromCharCode(e.keyCode).match(/[^0-9]/g)) return false;
+	});
+	
+
+	$('#enviar').click(function(){
+	
+		if ( !validarForm() )
+		{
+			alert("Por favor, complete todos los campos antes de enviar.");
+			return;
 		}
-    });
-	alert("Consulta enviada, muchas gracias");
-});
+		
+		
+		
+		var data = {
+			nombre: $("#nombre").val(),
+			email: $("#email").val(),
+			telefono: $("#telefono").val(),
+			mensaje: $("#mensaje").val()
+		};
+		$.ajax({
+			type: "POST",
+			url: "email.php",
+			data: data,
+			success: function(){
+				$('.success').fadeIn(1000);
+			}
+		});
+		
+		$("#nombre").val();
+		$("#email").val();
+		$("#telefono").val();
+		$("#mensaje").val();
+		alert("Consulta enviada, muchas gracias");
+		$('body,html').animate({scrollTop : 0}, 500);
+		
+	});
+
+
+function validarForm(){
+	
+	if( $("#nombre").val() =="" || $("#email").val() =="" || $("#telefono").val() =="" || $("#mensaje").val() =="" )
+		return 0;
+	
+	return 1;	
+}
 
 
 });
+
+
